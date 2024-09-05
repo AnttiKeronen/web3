498d450ea056ec6366784d27122179373e788eef
anttikeronen <156294651+AnttiKeronen@users.noreply.github.com>
2024-09-05 08:12:38 +0000

    juu

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("my-button").addEventListener("click", function () {
+  console.log("hello world");
+  document.getElementById("main-heading").textContent = "Moi maailma";
+});
+
+document.getElementById("add-data").addEventListener("click", function () {
+  const text = document.getElementById("custom-text").value;
+  if (text.trim() !== "") {
+    const li = document.createElement("li");
+    li.textContent = text;
+    document.getElementById("my-list").appendChild(li);
+    document.getElementById("custom-text").value = "";
+  } else {
+    alert("Please enter some text!");
+  }
+});
